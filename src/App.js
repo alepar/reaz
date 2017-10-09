175e99b64e84990a8b9f9f1e3062641d176e6671
Alexey Parfenov <parfenov@amazon.com>
2017-10-09 15:03:04 -0700

    react-data-grid with data stub

--- src/App.js
+++ src/App.js
@@ -1,21 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDataGrid from "react-data-grid";
+
+import data from "./data";
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+
+        this._columns = [
+            { key: 'name', name: 'Name', sortable: true },
+            { key: 'status', name: 'Status', width: 80, sortable: true },
+            { key: 'sizeBytes', name: 'Size', width: 120, sortable: true },
+            { key: 'uploadSpeedBytesPerSec', name: 'UL speed', width: 100, sortable: true },
+            { key: 'uploadedBytes', name: 'ULed', width: 120, sortable: true },
+            { key: 'availability', name: 'Avail.', width: 60, sortable: true },
+        ];
+
+        this.state = {
+            selectedIndexes: [],
+            rows: data.slice(),
+        };
+    }
+
+    handleGridSort(sortColumn, sortDirection) {
+        const comparer = (a, b) => {
+            if (sortDirection === 'ASC') {
+                return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+            } else if (sortDirection === 'DESC') {
+                return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+            }
+        };
+
+        const rows = sortDirection === 'NONE' ? data.slice() : this.state.rows.sort(comparer);
+
+        this.setState({
+            rows: rows,
+            selectedIndexes: this.state.selectedIndexes,
+        });
+    }
+
+    onRowsSelected(rows) {
+        this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
+    }
+
+    onRowsDeselected(rows) {
+        let rowIndexes = rows.map(r => r.rowIdx);
+        this.setState({selectedIndexes: this.state.selectedIndexes.filter(i => rowIndexes.indexOf(i) === -1 )});
+    }
+
+    rowGetter(i) {
+        return this.state.rows[i];
+    }
+
+    render() {
+        return (
+            <ReactDataGrid
+                rowKey={"key"}
+                columns={this._columns}
+                rowGetter={i => this.rowGetter(i)}
+                rowsCount={this.state.rows.length}
+                minHeight={750}
+                rowSelection={{
+                    showCheckbox: true,
+                    enableShiftSelect: true,
+                    onRowsSelected: r => this.onRowsSelected(r),
+                    onRowsDeselected: r => this.onRowsDeselected(r),
+                    selectBy: {
+                        indexes: this.state.selectedIndexes
+                    }
+                }}
+                onGridSort={(col, dir) => this.handleGridSort(col, dir)}
+            />
+        );
+    }
 }
 
 export default App;
