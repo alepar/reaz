e9441a4ddcc2a28a9963b46f39468e00507bd950
Alexey Parfenov <parfenov@amazon.com>
2017-10-09 23:23:31 -0700

    some formatting / styles

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from "react-redux";
 
@@ -10,11 +11,45 @@ class App extends React.Component {
         super(props);
 
         this._columns = [
-            { key: 'torrentName', name: 'Name', sortable: true },
-            { key: 'status', name: 'Status', width: 100, sortable: true },
-            { key: 'sizeBytes', name: 'Size', width: 120, sortable: true },
-            { key: 'uploadBps', name: 'UL speed', width: 100, sortable: true },
-            { key: 'uploadedBytes', name: 'ULed', width: 120, sortable: true },
+            {
+                key: 'torrentName',
+                name: 'Name',
+            },
+            {
+                key: 'status',
+                name: 'Status',
+                width: 100,
+                cellClass: "aligncenter",
+            },
+            {
+                key: 'done',
+                name: 'Done',
+                width: 60,
+                formatter: DoneFormatter,
+                cellClass: "aligncenter",
+                getRowMetaData: (row) => row
+            },
+            {
+                key: 'sizeBytes',
+                name: 'Size',
+                width: 120,
+                formatter: SizeFormatter,
+                cellClass: "alignright",
+            },
+            {
+                key: 'uploadBps',
+                name: 'UL speed',
+                width: 120,
+                formatter: SpeedFormatter,
+                cellClass: "alignright",
+            },
+            {
+                key: 'uploadedBytes',
+                name: 'ULed',
+                width: 120,
+                formatter: SizeFormatter,
+                cellClass: "alignright",
+            },
         ];
 
         if (0 === this.props.list.length) {
@@ -37,6 +72,7 @@ class App extends React.Component {
                     rowGetter={i => this.rowGetter(i)}
                     rowsCount={this.props.list.length}
                     minHeight={750}
+                    rowHeight={26}
                 />
             );
         } else {
@@ -57,6 +93,79 @@ class App extends React.Component {
             );
         }
     }
+}
+
+class SizeFormatter extends React.Component {
+    render() {
+        if (0 === this.props.value) {
+            return (
+                <div>-</div>
+            );
+        } else {
+            return (
+                <div>{sizeFormatter(this.props.value)}</div>
+            );
+        }
+    }
+}
+
+SizeFormatter.propTypes = {
+    value: PropTypes.number.isRequired
+};
+
+class SpeedFormatter extends React.Component {
+    render() {
+        if (0 === this.props.value) {
+            return (
+                <div>-</div>
+            );
+        } else {
+            return (
+                <div>{sizeFormatter(this.props.value)}/s</div>
+            );
+        }
+    }
+}
+
+SpeedFormatter.propTypes = {
+    value: PropTypes.number.isRequired
+};
+
+class DoneFormatter extends React.Component {
+    render() {
+        const row = this.props.dependentValues;
+        let pct = row.downloadedBytes / row.sizeBytes * 100
+        if (pct > 100) pct = 100;
+        pct = pct.toFixed(0);
+        return (
+            <div>{pct}%</div>
+        );
+    }
+}
+
+DoneFormatter.propTypes = {
+    value: PropTypes.number.isRequired
+};
+
+function sizeFormatter(value) {
+    let factor = 1;
+    value = Number(value);
+    while (value > 1024) {
+        factor *= 1024;
+        value /= 1024;
+    }
+    value = value.toFixed(2);
+    let suffix;
+    switch (factor) {
+        case 1: suffix = "B"; break;
+        case 1024: suffix = "kB"; break;
+        case 1024*1024: suffix = "MB"; break;
+        case 1024*1024*1024: suffix = "GB"; break;
+        case 1024*1024*1024*1024: suffix = "TB"; break;
+        case 1024*1024*1024*1024*1024: suffix = "PB"; break;
+        default: suffix = "0.oB"; break;
+    }
+    return `${value} ${suffix}`;
 }
 
 function mapStateToProps(state) {
